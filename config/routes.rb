dacaa5492cefdc621fa5a6d62dbffaca60d7b7b7
ogawa52045 <138991739+ogawa52045@users.noreply.github.com>
2023-12-25 15:35:45 +0000

    [finish]member_sign_in

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    get 'top' => 'homes#top'
    get 'about' => 'homes#about'
+   root to: 'homes#top'
 end
