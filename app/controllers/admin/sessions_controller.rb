4dc952f0b6efb87698e068e99dad6943a03b6a7f
ogawa52045 <138991739+ogawa52045@users.noreply.github.com>
2023-12-26 15:19:21 +0000

    [finish]admin_sign_in

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -18,10 +18,20 @@ class Admin::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+
+  
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
+  
+ protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_admin!, if: :admin_url
   
-  def admin_url
-    request.fullpath.include?("/admin")
-  end
-  
-  def after_sign_in_path_for(resource)
-    root_path
-  end
-
-  def after_sign_out_path_for(resource)
-    new_member_session_path
-  end
-  
-  protected
-  
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
 end
